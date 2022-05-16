65a0d126ef9aca35de4f53470a992827365d7f7f
Anderson Vizeu Osório <and.vizeu@hotmail.com>
2022-05-15 22:26:11 -0300

    Criação usecases;

---
+++ core/src/main/java/br/com/vestibular/core/usecase/vestibular/UpdateVestibularUseCase.java
@@ -0,0 +1,28 @@
+package br.com.vestibular.core.usecase.vestibular;
+
+import br.com.vestibular.core.domain.Vestibular;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class UpdateVestibularUseCase {
+
+    public Vestibular execute(final CreateVestibularUseCase.Request request) {
+        return null;
+    }
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+
+    }
+
+}
